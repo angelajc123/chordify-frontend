94e398003d20c458099df7d82f59a02bd0d84ca0
Angela Chen <angelajc@mit.edu>
2025-10-25 16:31:26 -0400

    validation function

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,5 @@
-// Shared constants for frontend and backend validation
+// Shared constants and functions for frontend and backend validation
+import { Chord, Key } from "tonal";
 
 // PlayBack constants
 export const INSTRUMENTS = ["Piano", "Guitar", "Synthesizer"];
@@ -9,3 +10,11 @@ export const MAX_SECONDS_PER_CHORD = 10;
 export const NUM_SUGGESTIONS = 24;
 export const GENRES = ["Pop", "Rock", "Jazz", "Classical", "Hip hop", "R&B", "Country", "Electronic"];
 export const COMPLEXITY_LEVELS = ["Simple", "Intermediate", "Advanced"];
+
+export function isValidChord(chord) {
+  return !Chord.get(chord).empty;
+}
+
+export function isValidKey(key) {
+  return Key.majorKey(key).tonic != "" || Key.minorKey(key).tonic != "";
+}
